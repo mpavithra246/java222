8a3180095fa30be8706b6df865108d3638f4e452
mpavithra246 <mpavithra246@gmail.com>
2016-08-08 11:50:18 +0530

    fdrg
    gdewuwajki

---
+++ numtoint.java
@@ -0,0 +1,21 @@
+public class MyStringToNumber {
+ 
+    public static int convert_String_To_Number(String numStr){
+         
+        char ch[] = numStr.toCharArray();
+        int sum1 = 0;
+        int zeroAscii = (int)'0';
+        for(char c:ch){
+            int tmpAscii = (int)c;
+            sum1 = (sum1*10)+(tmpAscii-zeroAscii);
+        }
+        return sum1;
+    }
+     
+    public static void main(String a[]){
+         
+        System.out.println("\"3256\" == "+convert_String_To_Number("3256"));
+        System.out.println("\"76289\" == "+convert_String_To_Number("76289"));
+        System.out.println("\"90087\" == "+convert_String_To_Number("90087"));
+    }
+}
